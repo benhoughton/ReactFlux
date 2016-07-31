dbcde138755991e5be5b6fbe5e02bd1fdfdbc630
benhoughton <bhoughton85@gmail.com>
2016-07-31 20:29:33 +0100

    Fixed the Eslint gulp plugin

--- gulpfile.js
+++ gulpfile.js
@@ -64,7 +64,7 @@ gulp.task('css', function(){
 
 gulp.task('lint', function(){
 	return gulp.src(config.paths.js)
-			.pipe(eslint({config: 'eslint.config.json'}))
+			.pipe(eslint({configFile: 'eslint.config.json'}))
 			.pipe(eslint.format());
 });
 
